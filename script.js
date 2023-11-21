a1b6fd4870026f6327bf8cb240b2489434659109
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-11-21 20:57:20 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function changeText() {
+    const element = document.getElementById("text");
+    const paragraph = document.getElementById("paragraph");
+
+    element.innerHTML = "This text has been changed";
+    paragraph.innerHTML = "How do you like them cookies!"
+}
